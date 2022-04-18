c194d66517411aedd75e12b8b3c63ce286631372
Akhil Raj <akhil.gs@gmail.com>
2022-04-17 21:05:23 -0400

    Initial Commit

---
+++ AkhilCard/AkhilCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkhilCardApp.swift
+//  AkhilCard
+//
+//  Created by Akhil Raj on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AkhilCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AkhilCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AkhilCard
+//
+//  Created by Akhil Raj on 4/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
